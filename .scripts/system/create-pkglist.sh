40820a4b136c4836d875dcac6f45bf7a6420ad60
Jake C <jake@jakec.dev>
2021-07-02 08:47:45 +1000

    Save pkglist without date in file name

--- .scripts/system/create-pkglist.sh
+++ .scripts/system/create-pkglist.sh
@@ -2,20 +2,19 @@
 
 outdir="/home/jake"
 backup_dir="/media/storage/backup"
-timestamp="$(date +"%Y-%m-%d_%H-%M")"
 pkglist="$(pacman -Qqen)"
 pkglist_aur="$(pacman -Qqem)"
 
 case $1 in
     pac-hook)
-        trash-put ${outdir}/pkglist*
-        echo "$pkglist" > ${outdir}/pkglist.${timestamp}.txt
-        echo "$pkglist_aur" > ${outdir}/pkglist-aur.${timestamp}.txt
+        trash-put ${outdir}/pkglist.txt ${outdir}/pkglist-aur.txt
+        echo "$pkglist" > ${outdir}/pkglist.txt
+        echo "$pkglist_aur" > ${outdir}/pkglist-aur.txt
         ;;
     backup)
-        trash-put ${backup_dir}/pkglist*
-        echo "$pkglist" > ${backup_dir}/pkglist.${timestamp}.txt
-        echo "$pkglist_aur" > ${backup_dir}/pkglist-aur.${timestamp}.txt
+        trash-put ${backup_dir}/pkglist.txt ${backup_dir}/pkglist-aur.txt
+        echo "$pkglist" > ${backup_dir}/pkglist.txt
+        echo "$pkglist_aur" > ${backup_dir}/pkglist-aur.txt
         ;;
     *)
         bold="$(tput bold)"
