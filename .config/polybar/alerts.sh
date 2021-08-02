160cdc988817fac1ab3bf65711dabd26f2f0a610
Jake C <jake@jakec.dev>
2021-08-02 20:08:37 +1000

    Set click-right on alerts module to toggle ALL notifications

--- .config/polybar/alerts.sh
+++ .config/polybar/alerts.sh
@@ -3,18 +3,9 @@
 readonly color_ok="#50fa7b"
 readonly color_alert="#ff5555"
 
-is_dead=$(systemctl --user status get-the-f-up.timer --no-pager | grep dead)
-
-function toggle_notify() {
-    if [[ ! -z $is_dead ]]; then
-        systemctl --user start get-the-f-up.timer
-    else
-        systemctl --user stop get-the-f-up.timer
-    fi
-}
-
 function poly_output() {
-    if [[ ! -z $is_dead ]]; then
+    is_paused=$(dunstctl is-paused)
+    if [[ $is_paused == true ]]; then
         echo "%{F$color_alert}"
     else
         echo "%{F$color_ok}"
@@ -23,7 +14,7 @@ function poly_output() {
 
 case "$1" in
     toggle)
-        toggle_notify
+        dunstctl set-paused toggle
         ;;
     show)
         dunstctl history-pop
