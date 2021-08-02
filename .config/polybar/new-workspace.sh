f95f296408a04209244cf482667ff8b4517c9fb6
Jake C <jake@jakec.dev>
2021-08-02 20:09:35 +1000

    Begin adding framework for updated version

--- .config/polybar/new-workspace.sh
+++ .config/polybar/new-workspace.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#workspaces_arr=$(i3-msg -t get_workspaces | jq)
+
+# check if workspace 1 is open
+#function check_ws_open() {
+    # for open_ws in $workspaces_arr; do
+
+    # done
+#}
+
+
 ## Open new workspace
 CURRENT_WS=$(i3-msg -t get_workspaces | jq length)
 NEW_WS=$(($CURRENT_WS+ 1))
