fc5e2c903d083e9b5b9f3cbb7550fdbfd40368b4
Jake C <jake@jakec.dev>
2021-08-06 15:28:44 +1000

    If a task has no project assigned then create the note for the task itself

--- .config/taskwarrior-tui/open-note.sh
+++ .config/taskwarrior-tui/open-note.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 task_uuid="$@"
-project_desc="$(task _get $task_uuid.project)"
-project_path="$(echo $project_desc | sed 's/\./\//g')"
-project_name="$(echo $project_path | awk -F '.' '{print $NF}')"
-project_uuid="$(task project:$task_uuid.project +project uuids)"
+task_project="$(task _get $task_uuid.project)"
 
-project_file="$HOME/Documents/Sync/vimwiki/tasks/$project_uuid.md"
-if [[ ! -f $project_file ]]; then
-    echo -e "# $project_name || project:$project_desc \
+if [[ $task_project ]]; then
+    file_name="$(task project:$task_project +project uuids)"
+    file_heading="$(echo $task_project | awk -F '.' '{print $NF}')"
+    file_template="# $file_heading || project:$project_desc \
         \n\n## Inbox | +inbox \
         \n\n## Next Actions | +next and status:pending \
         \n\n## Waiting | status:waiting \
         \n\n## Future | -VISIBLE  \
         \n\n## Completed | status:completed \
-        \n\n## Notes" > $project_file
-    task $project_uuid mod +notes
+        \n\n## Notes"
+else
+    file_name=$task_uuid
+    file_heading="$(task _get $task_uuid.description)"
+    file_template="# $file_heading \
+        \n\n## Notes"
 fi
 
-vim -c "VimwikiIndex" -c "VimwikiGoto tasks/$project_uuid"
+project_file="$HOME/Documents/Sync/vimwiki/tasks/$file_name.md"
+if [[ ! -f $project_file ]]; then
+    echo -e $file_template > $project_file
+    task $file_name mod +notes
+fi
+
+vim -c "VimwikiIndex" -c "VimwikiGoto tasks/$file_name"
